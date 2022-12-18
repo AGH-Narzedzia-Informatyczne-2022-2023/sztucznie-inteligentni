2d248d40235b6340bd106a268a3a7de1b07ba7d9
pfron <pavelfron4@gmail.com>
2022-12-18 23:42:55 +0100

    commented

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import pygame
 from pygame.locals import *
 
+# --- 1. Initializing
 pygame.init()
 
+# 1.1. Setting up display
 clock = pygame.time.Clock()
 fps = 60
-
 screen_width = 800
 screen_height = 920
-
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Flappy Bird')
+pygame.display.set_caption('Flappy Bird Clones')
+
+# 1.2. Loading background
+bg = pygame.image.load('img/bg.png')
+ground_img = pygame.image.load('img/ground.png')
  
-#define game variables
+# 1.3. Setting game variables
 ground_scroll = 0
 scroll_speed = 4
 animation_cooldown = 5
 
-#load images
-bg = pygame.image.load('img/bg.png')
-ground_img = pygame.image.load('img/ground.png')
-
-# create bird
+# --- 2. Player class
 class Player(pygame.sprite.Sprite):
+    # 2.1. Initializing the player
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
 
@@ -38,6 +39,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = [x, y]
     
     def update(self):
+        # 2.2. Flapping animation
         self.counter += 1
         if self.counter > animation_cooldown:
             self.counter = 0
@@ -47,28 +49,29 @@ class Player(pygame.sprite.Sprite):
             self.image = self.frames[self.index]
 
 
-
+# 2.3. Creating the player
 player_group = pygame.sprite.Group()
 player = Player(100, screen_width // 2)
 player_group.add(player)
 
+# --- 3. Game loop
 run = True
 while run:
 
     clock.tick(fps)
 
-    #draw background
+    # 3.1. Drawing and scrolling background
     screen.blit(bg, (0, 0))
-
-    player_group.draw(screen)
-    player_group.update()
-
-    #draw and scroll the ground
     screen.blit(ground_img, (ground_scroll, 768))
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 35:
         ground_scroll = 0
 
+    # 3.2. Drawing and animating the player
+    player_group.draw(screen)
+    player_group.update()
+
+    # 3.3. Listening for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
