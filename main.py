e32e9ddb122ba05b0dbad4925219595a0a95c54d
Radoslaw <115021620+Radekkedar@users.noreply.github.com>
2022-12-22 19:02:15 +0100

    Update main.py
    Kolejny update do kodu 
    coraz bardziej przypomina grę typu flappy bird

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ ground_img = pygame.image.load('img/ground.png')
 ground_scroll = 0
 scroll_speed = 4
 animation_cooldown = 5
+flying = False
+game_over = False
 
 # --- 2. Player class
 class Player(pygame.sprite.Sprite):
@@ -37,19 +39,43 @@ class Player(pygame.sprite.Sprite):
         self.image = self.frames[0]
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.vel = 0
+        self.clicked = False
     
     def update(self):
-        # 2.2. Flapping animation
-        self.counter += 1
-        if self.counter > animation_cooldown:
-            self.counter = 0
-            self.index += 1
-            if self.index > 2:
-                self.index = 0
+     
+        if flying == True:
+            # 2.2. Gravity 
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
+        
+        if game_over == False:
+            # 2.3. Jump 
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+     
+            # 2.4. Flapping animation
+            self.counter += 1
+            if self.counter > animation_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index > 2:
+                    self.index = 0
             self.image = self.frames[self.index]
+            
+            # 2.5. Rotate the player
+            self.image = pygame.transform.rotate(self.images[self.index], self.vel * -2)
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index], -90)
 
 
-# 2.3. Creating the player
+# 2.6. Creating the player
 player_group = pygame.sprite.Group()
 player = Player(100, screen_width // 2)
 player_group.add(player)
@@ -60,21 +86,30 @@ while run:
 
     clock.tick(fps)
 
-    # 3.1. Drawing and scrolling background
+    # 3.1. Drawing background
     screen.blit(bg, (0, 0))
     screen.blit(ground_img, (ground_scroll, 768))
-    ground_scroll -= scroll_speed
-    if abs(ground_scroll) > 35:
-        ground_scroll = 0
-
-    # 3.2. Drawing and animating the player
+    
+    # 3.2. Check if player has hit the ground 
+    if player.rect.bottom > 768:
+        game_over = True
+        flying = False
+    
+    if game_over == False:
+        ground_scroll -= scroll_speed
+        if abs(ground_scroll) > 35:
+            ground_scroll = 0
+         
+    # 3.3. Drawing and animating the player
     player_group.draw(screen)
     player_group.update()
 
-    # 3.3. Listening for events
+    # 3.4. Listening for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
 
     pygame.display.update()
 
